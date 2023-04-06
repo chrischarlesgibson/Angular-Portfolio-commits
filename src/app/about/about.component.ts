8a379d02e91a9105a6a22b3ca4e032d92e447bfa
Chris Gibson <chrischarlesgibson@gmail.com>
2023-04-06 12:57:09 -0500

    added about page and basic styling

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import {
+  faGithub,
+  faLinkedin,
+  faMedium,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
 })
-export class AboutComponent {}
+export class AboutComponent {
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+  faMedium = faMedium;
+}
