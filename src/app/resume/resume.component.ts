f598fe5b25e9759dd2914866925908a79ebaba05
Chris Gibson <chrischarlesgibson@gmail.com>
2023-04-06 18:39:44 -0500

    added resume page

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+const skills = [
+  'JavaScript',
+  'typeScript',
+  'C#',
+  '.NET',
+  'Visual Studio',
+  'jQuery',
+  'React',
+  'MySQL',
+  'HTML5',
+  'Node.js',
+  'Express.js',
+  'Jest',
+  'Handlebars',
+  'MongoDB',
+  'GraphQL',
+  'Sequelize',
+  'CSS3',
+  'Bootstrap',
+  'Bulma',
+  'Materialize',
+  'Heroku',
+  'Insomnia',
+];
+
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
-  styleUrls: ['./resume.component.css']
+  styleUrls: ['./resume.component.css'],
 })
 export class ResumeComponent {
-
+  public skills: string[] = skills;
+  resumeImage = '../../assets/images/resumeImage.png';
+  resumePDF = '../../assets/images/chrisGibsonResume.pdf';
+  constructor() {}
 }
